263d67c1f4b7346374478d334e8f568952655da5
Damian <37978110+dszyszek@users.noreply.github.com>
2019-09-29 15:44:36 +0200

    Add Profile model

--- server/server_app/models.py
+++ server/server_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=200)
+    images_source = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
