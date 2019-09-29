5eaf9a0d6fd6ba6bf0d225a838759290bcfae931
Damian <37978110+dszyszek@users.noreply.github.com>
2019-09-29 15:30:00 +0200

    Add GET profiles/ tests

--- server/server_app/tests.py
+++ server/server_app/tests.py
@@ -4,3 +4,27 @@ from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
 from .models import Profile
 from .serializers import ServerSerializer
+
+
+class ProfileInfoTest(APITestCase):
+    client = APIClient()
+
+    @staticmethod
+    def create_profile(name=''):
+        Profile.objects.create(name=name)
+    
+    def setUp(self):
+        self.create_profile('parrot')
+        self.create_profile('rope')
+        self.create_profile('shrek')
+    
+
+class GetProfileTest(ProfileInfoTest):
+
+    def get_all_profiles(self):
+        res = self.client.get(reverse('profiles'))
+
+        expected = Profile.objects.all()
+        serialized = ServerSerializer(expected, many=True)
+        self.assertEqual(res.data, serialized.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
