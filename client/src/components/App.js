83ab898df7976f34d9589d6d2ef38e73a8d54f2e
Damian <37978110+dszyszek@users.noreply.github.com>
2019-10-01 00:28:10 +0200

    Add client-side React app skeleton

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Some from './Some';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Some />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Some.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Some extends Component {
+    render() {
+        return (
+            <h1>Some component</h1>
+        );
+    }
+}
+
+export default Some;
