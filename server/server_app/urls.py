1b2df5305faa389a211f07ac473a2e558feb5871
Damian <37978110+dszyszek@users.noreply.github.com>
2019-10-01 00:22:26 +0200

    Catch every string from url as parameter

--- server/server_app/urls.py
+++ server/server_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+from . import views
 
-app_name = 'server_app'
+# app_name = 'server_app'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('profiles', views.profiles, name='profiles'),
+    path('<slug:profile_name>', views.ProfilesView.as_view(), name='ProfilesView')
 ]
