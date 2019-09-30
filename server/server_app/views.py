37ff22248f884cbb4306c67b8dc8f9173059c8d5
Damian <37978110+dszyszek@users.noreply.github.com>
2019-10-01 00:23:10 +0200

    Add ProfilesView view

--- server/server_app/views.py
+++ server/server_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Profile
+from .serializers import ProfileSerializer
+from .utils.scrape_images import scrape_images
 
-# Create your views here.
+class ProfilesView(generics.ListAPIView):
+    lookup_url_kwarg = 'profile_name'
+    serializer_class = ProfileSerializer
+
+
+    def get_queryset(self):
+        parameter_from_url = self.kwargs[self.lookup_url_kwarg]
+
+        try:
+            if Profile.objects.filter(name=parameter_from_url).exists():
+                queryset = Profile.objects.filter(name=parameter_from_url).values()
+            else:
+                raise Profile.DoesNotExist
+
+        except Profile.DoesNotExist:
+            new_profile_urls = scrape_images(parameter_from_url)
+            new_profile = Profile(name=parameter_from_url, images_source=new_profile_urls)
+            new_profile.save()
+
+            queryset = Profile.objects.filter(name=parameter_from_url).values()
+            
+        return queryset
