3b767521482c95938683ae4561fc555784306cc0
Damian <37978110+dszyszek@users.noreply.github.com>
2019-10-03 01:10:57 +0200

    Add MainContent component

---
+++ client/src/components/MainContent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledMainWrapper = styled.div`
+    background-color: #fafafa;
+    height: 88vh;
+`;
+
+
+
+class MainContent extends Component {
+    render() {
+        return (
+            <StyledMainWrapper>
+                
+            </StyledMainWrapper>
+        );
+    }
+}
+
+export default MainContent;
