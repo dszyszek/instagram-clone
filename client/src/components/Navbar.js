e1659bb9bb77ce05d8270278895b304e21353af0
Damian <37978110+dszyszek@users.noreply.github.com>
2019-10-02 23:46:06 +0200

    Apply instagram icon + use styled components

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,30 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+
+
+const StyledNav = styled.nav`
+    width: 100%;
+    height: 80px;
+    background-color: #fff;
+    display: flex;
+    border-bottom: 1px solid rgba(0,0,0,0.975);
+`;
+
+const StyledDiv = styled.div`
+    height: 36px;
+    width: 100%;
+    margin: 20.5px;
+    background-color: red;
+`;
 
 class Navbar extends Component {
     render() {
         return (
-            <h1>Navbar component</h1>
+            <StyledNav>
+                <StyledDiv>
+                    <i className='icon-instagram'></i>
+                </StyledDiv>
+            </StyledNav>
         );
     }
 }
