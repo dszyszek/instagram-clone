724224db9e9d5f8f50ebd743934e435b81076146
Damian <37978110+dszyszek@users.noreply.github.com>
2019-10-03 00:20:18 +0200

    Add static/ dir; add StyleLogoBox in Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 
+import logo from '../../static/instagram_logo.png'
 
 const StyledNav = styled.nav`
     width: 100%;
@@ -10,20 +11,39 @@ const StyledNav = styled.nav`
     border-bottom: 1px solid rgba(0,0,0,0.975);
 `;
 
-const StyledDiv = styled.div`
+const StyledWrapper = styled.div`
     height: 36px;
     width: 100%;
-    margin: 20.5px;
+    margin: 20.5px 12%;
     background-color: red;
+    display: flex;
+`;
+
+const StyledLogoBox = styled.div`
+    height: 100%;
+    width: 20%;
+    background-color: yellow;
+    display: flex;
+    align-items: center;
 `;
 
 class Navbar extends Component {
     render() {
         return (
             <StyledNav>
-                <StyledDiv>
-                    <i className='icon-instagram'></i>
-                </StyledDiv>
+                <StyledWrapper>
+                    <StyledLogoBox>
+                        <i className='icon-instagram' style={{
+                            fontSize: '28px',
+                            borderRight: '1px solid rgba(0,0,0,0.975)',
+                            paddingRight: '15px'
+                        }}></i>
+                        <img src={logo} style={{
+                            height: '40px',
+                            paddingLeft: '15px'
+                        }}></img>
+                    </StyledLogoBox>
+                </StyledWrapper>
             </StyledNav>
         );
     }
