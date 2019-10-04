67dadec16007d235c5b3390d817c417cf88703b4
Damian <37978110+dszyszek@users.noreply.github.com>
2019-10-05 00:27:36 +0200

    Return all profiles from db from Index view

--- server/server_app/views.py
+++ server/server_app/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.utils import timezone
 import random
+import namesgenerator
 
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -10,12 +11,10 @@ from .utils.scrape_images import scrape_images
 
 
 class Index(APIView):
-
     def get(self, req):
-        msg = {
-            'msg': 'Connected successfully to API'
-        }
-        return Response(msg)
+        queryset = Profile.objects.all()
+        serializer = ProfileSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class ProfilesView(generics.ListAPIView):
@@ -26,6 +25,7 @@ class ProfilesView(generics.ListAPIView):
         parameter_from_url = self.kwargs[self.lookup_url_kwarg]
         pub_date = timezone.now()
         likes = random.randint(0, 200)
+        nick = namesgenerator.get_random_name()
 
         try:
             if Profile.objects.filter(name=parameter_from_url).exists():
@@ -35,7 +35,7 @@ class ProfilesView(generics.ListAPIView):
 
         except Profile.DoesNotExist:
             new_profile_urls = scrape_images(parameter_from_url)
-            new_profile = Profile(name=parameter_from_url, images_source=new_profile_urls, pub_date=pub_date, likes=likes)
+            new_profile = Profile(name=parameter_from_url, images_source=new_profile_urls, pub_date=pub_date, likes=likes, nick=nick)
             new_profile.save()
 
             queryset = Profile.objects.filter(name=parameter_from_url).values()
