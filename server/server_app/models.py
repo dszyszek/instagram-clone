ba06b7ffdbd7b8f51c9e9ecedd5ebac975960743
Damian <37978110+dszyszek@users.noreply.github.com>
2019-10-04 11:34:25 +0200

    Add new fields in db model and its representation in views.py

--- server/server_app/models.py
+++ server/server_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Profile(models.Model):
     name = models.CharField(max_length=200)
     images_source = models.TextField(null=True)
+    pub_date = models.DateField(default=None)
+    likes = models.IntegerField(default=None)
 
     def __str__(self):
         return self.name
--- server/server_app/views.py
+++ server/server_app/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.utils import timezone
+import random
 
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -20,9 +22,10 @@ class ProfilesView(generics.ListAPIView):
     lookup_url_kwarg = 'profile_name'
     serializer_class = ProfileSerializer
 
-
     def get_queryset(self):
         parameter_from_url = self.kwargs[self.lookup_url_kwarg]
+        pub_date = timezone.now()
+        likes = random.randint(0, 200)
 
         try:
             if Profile.objects.filter(name=parameter_from_url).exists():
@@ -32,7 +35,7 @@ class ProfilesView(generics.ListAPIView):
 
         except Profile.DoesNotExist:
             new_profile_urls = scrape_images(parameter_from_url)
-            new_profile = Profile(name=parameter_from_url, images_source=new_profile_urls)
+            new_profile = Profile(name=parameter_from_url, images_source=new_profile_urls, pub_date=pub_date, likes=likes)
             new_profile.save()
 
             queryset = Profile.objects.filter(name=parameter_from_url).values()
