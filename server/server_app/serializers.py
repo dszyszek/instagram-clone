8086ddcff17a9f3244cd553f78c236f51c20050b
Damian <37978110+dszyszek@users.noreply.github.com>
2019-10-05 01:42:16 +0200

    Add PostSerializer

--- server/server_app/serializers.py
+++ server/server_app/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import Profile, Post
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta():
         model = Profile
         fields = ('name', 'images_source', 'pub_date', 'likes', 'nick')
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Post
+        fields = ('image', 'pub_date', 'likes', 'nick')
