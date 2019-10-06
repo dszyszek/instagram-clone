b946a9bdb64f4019f93f94509db6d92c22bdab19
Damian <37978110+dszyszek@users.noreply.github.com>
2019-10-06 02:13:44 +0200

    Render images; add styles

--- client/src/components/ProfileBody.js
+++ client/src/components/ProfileBody.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledProfileBody = styled.div`
+    display: flex;
+    width: 100%;
+    min-height: 100%;
+    flex-wrap: wrap;
+    margin-top: 30px;
+`;
+
+const StyledImg = styled.img`
+    margin: 1.5%;
+    width: 30%;
+    height: 300px;
+    border: 1px solid #e3e3e3;
+`;
+
 
 const ProfileBody = ({profile}) => {
+    const posts = JSON.parse(profile.images_source);
     return (
-        <h1>
-           Profile body 
-        </h1>
+        <StyledProfileBody>
+            {posts.map( (link, i) => (
+                <StyledImg key={i} src={link} />
+            ))}
+        </StyledProfileBody>
     );
 };
 
